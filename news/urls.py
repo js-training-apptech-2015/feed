17932ee38ec34aa6273882ba687294e52a459b7c
Ivan Gromov <summer.is.gone@gmail.com>
2013-12-09 01:11:35 +0600

    Added feed view

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-from news.views import EntryList, EntryDetail
-from views import EntryList
+from news.views import EntryList, EntryDetail, FeedList
 
 urlpatterns = patterns('',
     url(r'^$', EntryList.as_view(), name='entry_list'),
+    url(r'^feed/(?P<feed>[\d]+)/$', FeedList.as_view(), name='entry_feed_list'),
     url(r'^(?P<pk>[\d]+)/', EntryDetail.as_view(), name='entry_detail'),
 )
--- news/views.py
+++ news/views.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import ListView, DetailView
-from models import Entry
+from models import Entry, Feed
 
 
 class EntryList(ListView):
     model = Entry
 
+    def get_queryset(self):
+        return super(EntryList, self).get_queryset().order_by('published')
+
+class FeedList(ListView):
+    model = Entry
+    template_name = 'entry_feed_list.html'
+
+    def get_queryset(self):
+        return super(FeedList, self).get_queryset().filter(feed=self.kwargs['feed'])
+
+    def get_context_data(self, **kwargs):
+        context = super(FeedList, self).get_context_data(**kwargs)
+        context['feed'] = Feed.objects.get(id=self.kwargs['feed'])
+        return context
 
 class EntryDetail(DetailView):
     model = Entry
